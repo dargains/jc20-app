31908631f6931831afe404d9ebcfbe52c008b0a5
André Dargains <andre.dargains@gmail.com>
2020-08-10 16:31:40 +0100

    UPD: colors and screens;

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 module.exports = {
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1280px",
+      xl: "1440px",
+      xxl: "1920px"
+    },
     container: {
       center: true
     },
     extend: {
-    }
+      colors: {
+        green00: '#012b25',
+        green01: '#0e352f',
+        green02: '#193e38',
+        green03: '#32524d',
+        green04: '#4a6662',
+        green05: '#5d7572',
+        gren06: '#778a88',
+        green: '#278735',
+        red: '#fe0a01'
+      }
+    },
   },
   variants: {},
   plugins: [],
