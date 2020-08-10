b0ebb36f0fdee383bba432aab23b4a56815181d6
André Dargains <andre.dargains@gmail.com>
2020-08-10 21:22:12 +0100

    UPD: header icons, actions;

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,34 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { AppContext } from '../store.js';
+import Icon from './Icon'
+import Conditional from './Conditional.js';
 
 const Header = () => {
   const [state, dispatch] = useContext(AppContext);
-  const [language, setLanguage] = useState(state.language)
 
-  const handleChangeLang = e => {
-    dispatch({ type: 'CHANGE_LANGUAGE', payload: e.target.id })
-    setLanguage(state.language)
+  const handleToggleLang = lang => {
+    dispatch({ type: 'CHANGE_LANGUAGE', payload: lang })
+  }
+  const toggleMenu = () => {
+    dispatch({ type: 'TOGGLE_MENU' })
   }
   return (
-    <header className="container py-4">
-      <p className="text-xl">header</p>
-      <nav>
-        <Link className="mx-2 text-blue-400" to="/">main</Link>
-        <Link className="mx-2 text-blue-400" to="/home">home</Link>
-        <Link className="mx-2 text-blue-400" to="/units">units</Link>
-        <Link className="mx-2 text-blue-400" to="/share">share</Link>
+    <header className="container py-2 bg-green00 fixed bottom-0">
+      <nav className="flex w-full items-center justify-between px-4">
+        <Icon.Menu handleClick={toggleMenu} />
+        <Link className="mx-2 text-white" to="/share"><Icon.Share /></Link>
+        <Icon.User />
+        <div>
+          <Conditional if={state.language === 'pt'}>
+            <Icon.Lang lang={state.language} handleClick={() => { handleToggleLang('en') }} />
+          </Conditional>
+          <Conditional if={state.language === 'en'}>
+            <Icon.Lang lang={state.language} handleClick={() => { handleToggleLang('pt') }} />
+          </Conditional>
+        </div>
+        <Link className="mx-2 text-white" to="/home">home</Link>
       </nav>
-      <div>
-        <p>
-          language: {language}
-        </p>
-        <div>
-          <label htmlFor="pt">portugues</label>
-          <input id="pt" type="radio" value="pt" name="language" defaultChecked={language === 'pt'} onClick={handleChangeLang} />
-        </div>
-        <div>
-          <label htmlFor="en">english</label>
-          <input id="en" type="radio" value="en" name="language" defaultChecked={language === 'en'} onClick={handleChangeLang} />
-        </div>
-      </div>
     </header>
   )
 }
