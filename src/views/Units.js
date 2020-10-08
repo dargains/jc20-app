ca56363748acd67e8a49cacdb1886f3ea3ba1acb
André Dargains <andre.dargains@gmail.com>
2020-10-08 12:07:52 +0100

    UPD: units select styling;

--- src/views/Units.js
+++ src/views/Units.js
@@ -8,19 +8,6 @@ import { baseUrl } from '../api'
 import db from '../db'
 import fachada from '../assets/images/fachada.jpg'
 
-const getHeight = index => {
-  switch(index) {
-    case 0:
-    case 1:
-      return 85
-    case 2:
-    case 3:
-    case 4:
-      return 73
-    default:
-      return 150
-  }
-}
 const getStatus = status => {
   switch(status) {
     case 'available':
@@ -39,7 +26,10 @@ const Units = () => {
   const [units, setUnits] = useState([])
   const [unit, setUnit] = useState({})
   const ref = useRef()
-  useOnClickOutside(ref, () => setUnit({}));
+  useOnClickOutside(ref, e => {
+    if (e.target.closest(".apt")) return
+    else setUnit({});
+  });
   useEffect(() => {
     if (window.navigator.onLine) {
       console.log('Online. Fetching from CMS...')
@@ -73,46 +63,91 @@ const Units = () => {
     history.push(`/unit/${id}`)
   }
   return (
-    <section className={cx('apartments', 'bg-cover bg-no-repeat bg-center w-full h-screen')}>
+    <section
+      className={cx(
+        "apartments",
+        "bg-cover bg-no-repeat bg-center w-full min-h-screen"
+      )}
+    >
       <div className="h-24 bg-green07 flex items-center justify-between px-4">
-        {
-          unit.title
-          ? <div className={cx("w-full flex items-center justify-between",{'opacity-50': unit.status !== 'available'})}>
-              <p className="font-display text-white uppercase text-3xl">T{unit.bedrooms} <span className="text-green">{unit.extra}</span></p>
-              <div className="text-center text-white">
-                <p className="uppercase mb-2">Apartamento <span className="text-green">{unit.title}</span></p>
-                  <span className={cx(
-                    "text-xs py-1 px-4 border rounded-xl border-white text-white",
-                    {
-                      'bg-green01 border-green01 text-green07': unit.status === 'reserved'
-                    }
-                    )}>
-                    {getStatus(unit.status)}
-                  </span>
-              </div>
+        {unit.title ? (
+          <div
+            className={cx("w-full flex items-center justify-between", {
+              "opacity-75": unit.status !== "available",
+            })}
+          >
+            <p className="font-display text-white uppercase text-3xl">
+              T{unit.bedrooms} <span className="text-green">{unit.extra}</span>
+            </p>
+            <div className="text-center text-white">
+              <p className="uppercase mb-2">
+                Apartamento <span className="text-green">{unit.title}</span>
+              </p>
+              <span
+                className={cx(
+                  "text-xs py-1 px-4 border rounded-xl border-white text-white",
+                  {
+                    "bg-green01 border-green01 text-green07":
+                      unit.status === "reserved",
+                  }
+                )}
+              >
+                {getStatus(unit.status)}
+              </span>
             </div>
-          : <p className="text-2xl text-white uppercase font-display font-medium">Escolha o seu <span className="text-green font-light">apartamento</span></p>
-        }
+          </div>
+        ) : (
+          <p className="text-2xl text-white uppercase font-display font-medium">
+            Escolha o seu{" "}
+            <span className="text-green font-light">apartamento</span>
+          </p>
+        )}
       </div>
-      <Building className="building px-6 grid py-6" ref={ref} style={{backgroundImage: `url(${fachada})`}}>
-        {units.map((unit, index) => 
-          <AptImage key={unit.id}  index={index} onClick={() => selectUnit(unit)}>
+      <Building
+        className="grid"
+        style={{ padding: "10% 8% 8%", backgroundImage: `url(${fachada})` }}
+      >
+        {units.map((unit, index) => (
+          <AptImage
+            key={unit.id}
+            index={index}
+            ref={ref}
+            onClick={() => selectUnit(unit)}
+            className="apt"
+          >
             <img src={unit.thumbnail.data.full_url} alt={unit.title} />
-          </AptImage>)}
+          </AptImage>
+        ))}
       </Building>
-
-    </section >
-  )
+      {/* <button
+        className="mt-4 p-2 bg-green text-white"
+        onClick={() => db.delete()}
+      >
+        limpar
+      </button> */}
+      <svg height="0" xmlns="http://www.w3.org/2000/svg">
+        <filter id="drop-shadow">
+          <feGaussianBlur in="SourceAlpha" stdDeviation="6" />
+          <feOffset dx="0" dy="0" result="offsetblur" />
+          <feFlood floodColor="rgba(0,0,0,0.7)" />
+          <feComposite in2="offsetblur" operator="in" />
+          <feMerge>
+            <feMergeNode />
+            <feMergeNode in="SourceGraphic" />
+          </feMerge>
+        </filter>
+      </svg>
+    </section>
+  );
 }
 
 const Building = styled.div`
-  grid-template-columns: 1.2fr repeat(4, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   justify-items: center;
-  max-width: 730px;
+  max-width: 768px;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  height: 670px;
   figure:nth-child(1) {
     grid-column: span 2;
     justify-self: right;
@@ -127,24 +162,33 @@ const Building = styled.div`
   figure:nth-child(6) {
     grid-column: span 5;
   }
+  figure:nth-child(7),
+  figure:nth-child(8) {
+    grid-column: span 2;
+  }
+  figure:nth-child(8) {
+    grid-column: 4/6;
+  }
   &:hover {
     figure {
       opacity: 0.8;
     }
   }
-`
+`;
 
 const AptImage = styled.figure`
-  // height: ${props => getHeight(props.index)}px;
-  transition: all .2s ease-in-out;
+  transition: all 0.2s ease-in-out;
   will-change: transform;
   &:hover {
     opacity: 1 !important;
-    box-shadow: 0 3px 24px rgba(0,0,0,.7);
-    transform: scale(1.05);
+    -webkit-filter: drop-shadow(0 3px 25px rgba(0, 0, 0, 0.7));
+    filter: url(#drop-shadow);
+    -ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444')";
+    filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444')";
+    transform: scale(1.1);
     z-index: 1;
   }
-`
+`;
 
 
 export default Units
