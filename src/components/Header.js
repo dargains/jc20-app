ccb36f3617cef8a0080ee084482ea1789665b124
André Dargains <andre.dargains@gmail.com>
2020-10-08 23:40:36 +0100

    UPD: whatsapp number on desktop;

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,56 +36,95 @@ const Header = () => {
   return (
     <header className="bg-green08 fixed bottom-0 z-20 w-screen">
       <nav className="container py-2 bg-green08 flex w-full items-center z-20 justify-between px-4">
-        <HeaderIcon name="menu" handleClick={toggleMenu} className={cx({"opacity-50": state.menuIsOpen})}>
+        <HeaderIcon
+          name="menu"
+          handleClick={toggleMenu}
+          className={cx({ "opacity-50": state.menuIsOpen })}
+        >
           <Icon.Menu />
         </HeaderIcon>
         <HeaderIcon name="partilha">
-          <Link className="mx-2" to="/share"><Icon.Share /></Link>
+          <Link className="mx-2" to="/share">
+            <Icon.Share />
+          </Link>
         </HeaderIcon>
         <HeaderIcon name="perfil">
           <Icon.User />
         </HeaderIcon>
         <div>
-          <Conditional if={state.language === 'pt'}>
-            <HeaderIcon name="idioma" handleClick={() => handleToggleLang('en')} >
+          <Conditional if={state.language === "pt"}>
+            <HeaderIcon
+              name="idioma"
+              handleClick={() => handleToggleLang("en")}
+            >
               <Icon.Lang lang={state.language} />
             </HeaderIcon>
           </Conditional>
-          <Conditional if={state.language === 'en'}>
-            <HeaderIcon name="idioma" handleClick={() => handleToggleLang('pt')} >
-              <Icon.Lang lang={state.language}/>
+          <Conditional if={state.language === "en"}>
+            <HeaderIcon
+              name="idioma"
+              handleClick={() => handleToggleLang("pt")}
+            >
+              <Icon.Lang lang={state.language} />
             </HeaderIcon>
           </Conditional>
         </div>
         <div className="w-12 h-10" onClick={() => setShowDialog(!showDialog)}>
-          <div className="absolute w-32 h-32" style={{top: -60, left: -40}}>
-            <img src={whatsappImage} alt="Whatsapp"/>
+          <div className="absolute w-32 h-32" style={{ top: -60, left: -40 }}>
+            <img src={whatsappImage} alt="Whatsapp" />
           </div>
         </div>
       </nav>
 
-      <aside className={cx(
-        "fixed top-0 left-0 h-full w-full z-10 bg-white bg-opacity-50 pointer-events-none opacity-0 transition-opacity duration-300",
-        {"pointer-events-auto opacity-100": showDialog}
-        )}>
-        <div className={cx("bg-white pt-16 pb-10 rounded-t-full w-full absolute text-center transform transition-transform duration-300 delay-150", {"translate-y-4": !showDialog})} style={{bottom: 58}}>
-          <Icon.Close className="text-green08 absolute transform -translate-x-1/2 scale-75" style={{top: 20, left: '50%'}} handleClick={() => setShowDialog(false)} />
-          <p className={cx(
-            "text-green08 mb-6 font-display text-3xl transition-all  transform duration-200 delay-200",
-            {"opacity-0 translate-y-4": !showDialog}
-            )}>Precisa de <span className="text-green">ajuda?</span></p>
-          <p className={cx(
-            "text-md text-green08 mb-8 transition-all duration-200 delay-200",
-            {"opacity-0 translate-y-4": !showDialog}
-            )}>Vamos conversar por WhatsApp!</p>
-          <Button text="abrir conversa" type="primary" className={cx(
-            "transition-all duration-200 delay-200",
-            {"opacity-0 transform-y-4": !showDialog}
-            )} handleClick={handleContact}/>
+      <aside
+        className={cx(
+          "fixed top-0 left-0 h-full w-full z-10 bg-white bg-opacity-50 pointer-events-none opacity-0 transition-opacity duration-300",
+          { "pointer-events-auto opacity-100": showDialog }
+        )}
+      >
+        <div
+          className={cx(
+            "bg-white pt-16 pb-10 rounded-t-full w-full absolute text-center transform transition-transform duration-300 delay-150",
+            { "translate-y-4": !showDialog }
+          )}
+          style={{ bottom: 58 }}
+        >
+          <Icon.Close
+            className="text-green08 absolute transform -translate-x-1/2 scale-75"
+            style={{ top: 20, left: "50%" }}
+            handleClick={() => setShowDialog(false)}
+          />
+          <p
+            className={cx(
+              "text-green08 mb-6 font-display text-3xl transition-all  transform duration-200 delay-200",
+              { "opacity-0 translate-y-4": !showDialog }
+            )}
+          >
+            Precisa de <span className="text-green">ajuda?</span>
+          </p>
+          <p
+            className={cx(
+              "text-md text-green08 mb-8 transition-all duration-200 delay-200",
+              { "opacity-0 translate-y-4": !showDialog }
+            )}
+          >
+            Vamos conversar por WhatsApp!
+          </p>
+          <Button
+            text="abrir conversa"
+            type="primary"
+            className={cx("transition-all duration-200 delay-200 md:hidden", {
+              "opacity-0 transform-y-4": !showDialog,
+            })}
+            handleClick={handleContact}
+          />
+          <p className="hidden md:block font-display text-4xl text-green08 font-bold">
+            +351 <span className="text-green">978 456 432</span>
+          </p>
         </div>
       </aside>
     </header>
-  )
+  );
 }
 
 export default Header
