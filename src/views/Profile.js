5f5d7b77afcf018ca66ab490106aace19511112e
André Dargains <andre.dargains@gmail.com>
2020-10-19 23:15:55 +0100

    UPD: profile page;

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Icon from '../components/Icon'
 import Mask from '../components/Mask'
 
 const Profile = () => {
@@ -9,6 +10,14 @@ const Profile = () => {
         <h1 className=" font-display text-4xl font-semibold w-2/3 mb-8">
           Aceda ao seu <br/><span className="text-green">Perfil</span>
         </h1>
+        <article className="bg-white rounded-xl shadow-lg flex flex-col items-center justify-center py-8">
+          <Icon.Construction height={64} />
+          <p className="text-green08 text-2xl mt-4">Cliente</p>
+        </article>
+        <article className="bg-white rounded-xl shadow-lg flex flex-col items-center justify-center py-8 my-8">
+          <Icon.Construction height={64} />
+          <p className="text-green08 text-2xl mt-4">Consultor</p>
+        </article>
       </div>
     </section>
   )
