99bd21533adafa649a4ad36ac7deff86259897f6
André Dargains <andre.dargains@gmail.com>
2020-11-03 15:46:40 +0000

    UPD: packages;

--- src/views/EditProfile.js
+++ src/views/EditProfile.js
@@ -111,7 +111,7 @@ const EditProfile = () => {
               />
               {errors.password && <ErrorMessage>Este campo é obrigatório</ErrorMessage>}
 
-              {/* <select
+              <select
                 name="apartment"
                 ref={register({required: true})}
                 className={cx(
@@ -140,9 +140,9 @@ const EditProfile = () => {
                     </>
                   }
               </select>
-              {errors.apartment && <ErrorMessage>Este campo é obrigatório</ErrorMessage>} */}
+              {errors.apartment && <ErrorMessage>Este campo é obrigatório</ErrorMessage>}
 
-              {/* <Inputbox
+              <Inputbox
                 type="password"
                 color="green"
                 placeholder={type === 'agent' ? "código imobiliária" : "código cliente"}
@@ -150,7 +150,7 @@ const EditProfile = () => {
                 error={errors.code}
                 register={register({required: true})}
               />
-              {errors.code && <ErrorMessage>Este campo é obrigatório</ErrorMessage>} */}
+              {errors.code && <ErrorMessage>Este campo é obrigatório</ErrorMessage>}
 
               <p className="text-red mt-4 text-xs">{errorMessage}</p>
               <Button text="guardar" type="primary" className="mt-10" />
