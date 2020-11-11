460095ebfdb4e52f059422e5f9d969d541cde793
André Dargains <andre.dargains@gmail.com>
2020-11-11 16:18:32 +0000

    UPD: gallery translations;

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,8 +1,15 @@
-import React, {useState} from 'react'
+import React, { useState, useContext, useEffect, useCallback } from 'react'
 import Mask from '../components/Mask';
 import ImageOverlay from '../components/ImageOverlay';
+import Axios from 'axios';
+import { AppContext } from '../store.js';
+import { baseUrl } from '../api'
+import db from '../db'
 
 const Gallery = () => {
+  const [state] = useContext(AppContext);
+  const [content, setContent] = useState({})
+  const [copy, setCopy] = useState({copy:{}, language: state.language})
   const [showImage, setShowImage] = useState(false)
   const [selectedItem, setSelectedItem] = useState(0)
   const images = [
@@ -62,13 +69,45 @@ const Gallery = () => {
     setShowImage(true);
     setSelectedItem(images.indexOf(image))
   }
+
+  const changeCopy = useCallback(content => {
+    const copy = content.find(translation => translation.language === state.language)
+    setCopy({copy, lang: state.language})
+  },[state.language])
+  
+  useEffect(() => {
+    if (!Object.keys(content).length) {
+      if (window.navigator.onLine) {
+        Axios(`${baseUrl}/page_gallery?fields=*.*`).then(response => {
+          const allContent = response.data.data[0].translations;
+          setContent(allContent)
+          db.content.put({ page: 'Gallery', content: allContent })
+          changeCopy(allContent)
+        })
+      } else {
+        db.content.get('Gallery').then(contentDB => {
+          setContent(contentDB.content)
+          changeCopy(contentDB.content)
+        })
+      }
+    } else {
+      if (state.language !== copy.lang) {
+        changeCopy(content)
+      }
+    }
+    return () => {
+      
+    }
+  }, [changeCopy, content, copy.lang, state.language])
+
+
   return (
     <section>
       <Mask />
       <div className="wrapper">
-        <h1 className=" font-display text-4xl font-semibold w-2/3 mb-8">
-          <span className="text-green">Avenida Living</span><br/> na primeira linha do requinte
-        </h1>
+        <h1 className=" font-display text-4xl font-semibold w-2/3 mb-8 text-black"dangerouslySetInnerHTML={{
+          __html: copy.copy.text
+        }} />
         <div className="grid grid-cols-2 gap-2">
           {images.map(image => <img key={image.src} src={image.src} alt={image.alt} className={image.className} onClick={() => selectImage(image)} />)}
         </div>
