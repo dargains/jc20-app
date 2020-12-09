1ac0f2d17eaff94f93e13086cf9c9b3d3d499616
André Dargains <andre.dargains@gmail.com>
2020-12-09 12:38:11 +0000

    FIX: gallery image size;

--- src/components/ImageOverlay.js
+++ src/components/ImageOverlay.js
@@ -8,7 +8,7 @@ import { Carousel } from "react-responsive-carousel";
 const Slide = ({src, alt}) => {
   return (
     <div className="flex-1 flex flex-col">
-      <img className="h-64" src={src} alt={alt} />
+      <img src={src} alt={alt} style={{maxHeight: '70vh'}} />
       <div
         className="mt-12 text-left text-green08 px-6"
         dangerouslySetInnerHTML={{ __html: alt }}
@@ -22,8 +22,8 @@ const ImageOverlay = ({src, alt, images = [], selectedItem, showImage, handleClo
   return (
     <aside className={cx("text-white w-screen h-full fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center transition-opacity opacity-0 duration-200 pointer-events-none z-30", {"opacity-100 pointer-events-auto": showImage}, className)}>
       <div className="w-screen h-full bg-gray-900 opacity-90 absolute top-0 left-0"></div>
-      <div className="flex flex-col w-full items-start">
-        <Icon.Close className="mb-4 self-end place-self-end mr-4" handleClick={handleClose}/>
+      <div className="flex flex-col w-full items-center mt-2">
+        <Icon.Close className="mb-4 mr-4 self-end place-self-end" handleClick={handleClose}/>
         {
           !!images.length
           ? <Carousel
@@ -43,13 +43,13 @@ const ImageOverlay = ({src, alt, images = [], selectedItem, showImage, handleClo
               ))}
             </Carousel>
           : <figure
-              className={cx("w-full", {"p-4 bg-white": spaced})}
+              className={cx("w-full")}
               style={{maxHeight: '80vh'}}
             >
               <img
                 src={src}
                 alt={alt}
-                className="h-full object-contain"
+                className={cx("h-full object-contain",{"p-4 bg-white": spaced})}
               />
               <figcaption className="px-4 mt-2 text-xs">{alt}</figcaption>
             </figure>
