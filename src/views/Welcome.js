7ecd98c5c06c4fa48146ad336a89971efab93c52
André Dargains <andre.dargains@gmail.com>
2020-12-13 18:12:30 +0000

    UPD: ios/android welcome pwa message;

--- src/views/Welcome.js
+++ src/views/Welcome.js
@@ -1,22 +1,29 @@
 import React, {useState, useContext, useEffect, useCallback} from 'react'
 import { AppContext } from '../store.js';
+import cx from 'classnames'
+import Axios from 'axios';
+import { itemsUrl } from '../api.js';
+import db from '../db.js';
+import Button from '../components/Button.js';
 
 import Image01 from '../assets/images/cam-03-varanda.jpg'
 import Image02 from '../assets/images/cam-04-hall.jpg'
 import Image03 from '../assets/images/cam-07-escadas.jpg'
 import Image04 from '../assets/images/cam-08-wc.jpg'
-import Axios from 'axios';
-import { itemsUrl } from '../api.js';
-import db from '../db.js';
+import Icon from '../components/Icon.js';
+import { isIOS } from '../helpers.js';
 
 const images = [Image01,Image02,Image03,Image04]
 const RandomImage = images[Math.floor(Math.random() * Math.floor(3)) + 1]
 
 const Welcome = () => {
   const [state, dispatch] = useContext(AppContext);
+  const [showDialog, setShowDialog] = useState(true)
   const [name, setName] = useState('')
   const [content, setContent] = useState({})
   const [copy, setCopy] = useState({})
+
+  const ios = isIOS();
 
   const changeCopy = useCallback(content => {
     const copy = content.find(translation => translation.language === state.language)
@@ -34,6 +41,11 @@ const Welcome = () => {
     }, 3000);
   },[dispatch, state.user])
 
+  const handleClose = () => {
+    setShowDialog(false)
+    timeout()
+  }
+
   useEffect(() => {
     if (!Object.keys(content).length) {
       if (window.navigator.onLine) {
@@ -42,19 +54,16 @@ const Welcome = () => {
           setContent(allContent)
           db.content.put({ page: 'Welcome', content: allContent })
           changeCopy(allContent)
-          timeout()
         })
       } else {
         db.content.get('Welcome').then(contentDB => {
           setContent(contentDB.content)
           changeCopy(contentDB.content)
-          timeout()
         })
       }
     } else {
       if (state.language !== copy.lang) {
         changeCopy(content)
-        timeout()
       }
     }
     
@@ -75,6 +84,52 @@ const Welcome = () => {
           {name || copy.welcome}
         </h2>
       </div>
+      <aside
+        className={cx(
+          "absolute top-0 left-0 h-full w-full z-10 bg-white bg-opacity-50 pointer-events-none opacity-0 transition-opacity duration-300",
+          { "pointer-events-auto opacity-100": showDialog }
+        )}
+      >
+        <div
+          className={cx(
+            "bg-white pt-16 pb-10 rounded-t-full w-full shadow-lg absolute text-center transform transition-transform duration-300 delay-150",
+            { "translate-y-4": !showDialog }
+          )}
+          style={{ bottom: 58, backgroundImage: 'linear-gradient(to bottom, #d3d7d6, #ffffff' }}
+        >
+          <Icon.Close
+            className="text-green08 absolute transform -translate-x-1/2 scale-75"
+            style={{ top: 20, left: "50%" }}
+            handleClick={handleClose}
+          />
+          <p
+            className={cx(
+              "text-green08 mb-6 font-display text-4xl w-2/3 mx-auto transition-all transform duration-200 delay-200",
+              { "opacity-0 translate-y-4": !showDialog }
+            )}
+          >
+            Adicione o <span className="text-green">Avenida Living</span> ao seu ecrã principal
+          </p>
+          {
+            ios
+            ? <p
+              className={cx(
+                "text-md text-green08 mb-8 transition-all duration-200 delay-200",
+                { "opacity-0 translate-y-4": !showDialog }
+              )}
+            >
+              Clique em <Icon.Homescreen className="inline-block mx-1" /> e depois em "Ecrã principal"
+            </p>
+          : <Button
+              text="abrir conversa"
+              type="primary"
+              className={cx("transition-all duration-200 delay-200 md:hidden", {
+                "opacity-0 transform-y-4": !showDialog,
+              })}
+            />
+          }
+        </div>
+      </aside>
     </section>
   )
 }
