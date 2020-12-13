2266b800593546fa66079250f3196bc3eff817f5
André Dargains <andre.dargains@gmail.com>
2020-12-13 15:29:48 +0000

    UPD: register duplicate;

--- src/views/ClientRegister.js
+++ src/views/ClientRegister.js
@@ -2,26 +2,38 @@ import Axios from 'axios';
 import React, { useState } from 'react'
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
+import cx from 'classnames'
 import { itemsUrl } from '../api';
 import Button from '../components/Button';
 import Inputbox from '../components/Inputbox';
 import Mask from '../components/Mask'
+import Icon from '../components/Icon';
 import { useContext } from 'react';
 import { AppContext } from '../store';
+import { shareMobile } from '../helpers';
+import { useHistory } from 'react-router-dom';
 
 const ClientRegister = () => {
   const [errorMessage, setErrorMessage] = useState('')
   const [emailSent, setEmailSent] = useState(false)
+  const [duplicatedClientError, setDuplicatedClientError] = useState(false)
   const { register, handleSubmit, errors } = useForm();
   const [state] = useContext(AppContext)
+  const history = useHistory()
 
   const onSubmit = async data => {
     const headers = {
       Authorization: `Bearer ${state.user.token}`
     }
     try {
-      await Axios.post(`${itemsUrl}/clients`, data, {headers})
-      setEmailSent(true)
+      const check = await Axios(`${itemsUrl}/clients?filter[name]=${data.name}`, {headers})
+      if (check.data.data.length) {
+        setDuplicatedClientError(true)
+      } else {
+        await Axios.post(`${itemsUrl}/clients`, data, {headers})
+        setEmailSent(true)
+        shareMobile()
+      }
     } catch (error) {
       setErrorMessage(error.response.data.error.message)
     }
@@ -87,6 +99,57 @@ const ClientRegister = () => {
           </form>
         }
       </div>
+
+
+
+      {
+        duplicatedClientError &&
+      <aside
+        className={cx(
+          "absolute top-0 left-0 h-full w-full z-10 bg-white bg-opacity-50 pointer-events-none opacity-0 transition-opacity duration-300",
+          { "pointer-events-auto opacity-100": duplicatedClientError }
+        )}
+      >
+        <div
+          className={cx(
+            "pt-16 pb-10 rounded-t-full w-full absolute text-center bottom-0 transform transition-transform duration-300 delay-150",
+            { "translate-y-4": !duplicatedClientError }
+          )}
+          style={{ backgroundImage: "linear-gradient(to bottom, #d3d7d6, #ffffff" }}
+        >
+          <Icon.Close
+            className="text-green08 absolute transform -translate-x-1/2 scale-75"
+            style={{ top: 20, left: "50%" }}
+            handleClick={() => setDuplicatedClientError(false)}
+          />
+          <p
+            className={cx(
+              "text-green05 mb-6 font-bold text-lg w-1/2 mx-auto transition-all  transform duration-200 delay-200",
+              { "opacity-0 translate-y-4": !duplicatedClientError }
+            )}
+          >
+            Ops… esse cliente já se encontra registado por outro consultor
+          </p>
+          <p
+            className={cx(
+              "text-md text-green08 mb-8 w-3/4 mx-auto transition-all duration-200 delay-200",
+              { "opacity-0 translate-y-4": !duplicatedClientError }
+              )}
+          >
+            Favor entrar em contato com a Rio Capital para maiores esclarecimentos
+          </p>
+          <Button
+            text="contatos"
+            type="primary"
+            className={cx("transition-all duration-200 delay-200", {
+              "opacity-0 transform-y-4": !duplicatedClientError,
+            })}
+            handleClick={() => {history.push('/contacts')}}
+          />
+        </div>
+      </aside>
+      }
+
     </section>
   )
 }
